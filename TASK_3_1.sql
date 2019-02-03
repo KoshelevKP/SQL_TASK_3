8aa9d0c642a48588fa2991963b5be56c84c15439
KoshelevKP <43110471+KoshelevKP@users.noreply.github.com>
2019-02-04 00:34:57 +0300

    Add files via upload

--- TASK_3_1.sql
+++ TASK_3_1.sql
@@ -1,5 +1,10 @@
-cd \netology_data
-vagrant up
-vagrant ssh
-sudo su -l postgres
-psql -U postgres
+SELECT userId, movieId, rating,
+    (rating - MIN(rating) OVER (PARTITION BY userId))/((MAX(rating) OVER (PARTITION BY userId)) - (MIN(rating) OVER (PARTITION BY userId))) normed_rating,
+    AVG(rating) OVER (PARTITION BY userId) avg_rating
+FROM (
+    SELECT DISTINCT
+        userId, movieId, rating
+    FROM ratings
+    WHERE userId <> 1 LIMIT 1000
+) as sample
+ORDER BY userId, rating DESC LIMIT 30;
